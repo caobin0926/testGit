f82145fcaddc2a863ac824d5cd8e2536484c58cf
caobin <caobin@lizhi.fm>
2022-02-15 22:49:25 +0800

    修改第二个文件

--- Test.java
+++ Test.java
@@ -7,7 +7,10 @@
 public class Test {
 
     public static void main(String [] args){
+        int num=3;
         System.out.println("第一个文件");
         System.out.println("1");
+
+        String b ="测试";
     }
 }
