1070dd42c3d16603c0bcc825b02b826da1f8d902
caobin <caobin@lizhi.fm>
2022-02-15 22:46:51 +0800

    修改第一个文件

--- Test.java
+++ Test.java
@@ -7,7 +7,9 @@
 public class Test {
 
     public static void main(String [] args){
+        int numer=1;
         System.out.println("第一个文件");
         System.out.println("1");
+        String a="ceshi";
     }
 }
